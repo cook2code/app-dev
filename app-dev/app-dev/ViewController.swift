603ba965df3c6d2048f128a44cd4b4585bb15fed
Colin Cook <Colin@Colin-Cooks-MacBook-Pro.local>
2017-11-28 13:16:07 -0800

    initial xcode project

---
+++ app-dev/app-dev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-dev
+//
+//  Created by Colin Cook on 11/28/17.
+//  Copyright © 2017 Colin Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
